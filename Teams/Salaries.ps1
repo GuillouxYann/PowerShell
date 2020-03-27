48fa1164e3c906ced1f6cd0ad3c8c6bafd5a417d
GUILLOUX Yann <yguilloux@cos-nanteau.local>
2020-03-27 21:54:57 +0100

    refactoring

--- Teams/Salaries.ps1
+++ Teams/Salaries.ps1
@@ -1,21 +1,36 @@
-#Récupère la liste des salariés du CRPF
+
+
+#Connexion au domaine
+Try {
+    Test-Connection   "vad3.cos-nanteau.local"
+}
+Catch {
+    Write-Host "impossible de se connecter au domaine"
+}
+
+#Récupération des Utilisateurs CRPF
 $OU = "OU=Users CRPF,OU=Utilisateurs,DC=cos-nanteau,DC=local"
 $CRPF = Get-ADUser -Filter "*" -SearchBase $OU -Properties city
-
 $users =  $CRPF | Select-Object samaccountname | Sort-Object samaccountname
 
-Connect-MicrosoftTeams
+#Connexion à TEAMS 
+try {
+    Connect-MicrosoftTeams
+    }
+}
+catch {
+    Write-Host "Impossible de se connecter à Teams"
+}    
+
 $testYG = New-Team -DisplayName "TESTYG" -Visibility "private" -Owner "yguilloux@cosformation.fr"
 $id = $testYG.GroupId 
 
-
 foreach ($user in $users) 
-{   $mail = $user.samAccountName+"@cosformation.fr"
+    {   $mail = $user.samAccountName+"@cosformation.fr"
     try {
         Add-TeamUser -GroupId $id -User $mail -Role "member"
-    }
+        }
     catch {
         $pasDeCompte += $user.samAccountName
+        }
     }
-}
- 
