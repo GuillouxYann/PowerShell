7a697b35677c82e647ad9e5cba4cc68c431f9342
GUILLOUX Yann <yguilloux@cos-nanteau.local>
2020-03-27 13:57:50 +0100

    Teams

--- Exchange 365/Teams/Salaries.ps1
+++ Exchange 365/Teams/Salaries.ps1
@@ -1,5 +1,14 @@
+#Récupère la liste des salariés du CRPF
 $OU = "OU=Users CRPF,OU=Utilisateurs,DC=cos-nanteau,DC=local"
 $CRPF = Get-ADUser -Filter "*" -SearchBase $OU -Properties city
 
-$Logins =  $CRPF | Select-Object samaccountname | Sort-Object  
-$Logins | Format-Table  
+$users =  $CRPF | Select-Object samaccountname | Sort-Object samaccountname
+
+Connect-MicrosoftTeams
+$TestYG = New-Team -DisplayName "TESTYG" -Visibility "private" -Owner "yguilloux@cosformation.fr"
+$ID = $TestYG.GroupId 
+
+foreach ($user in $users) 
+{   $mail = $user+"@cosformation.fr"
+    Add-TeamUser -GroupId $ID -User $mail -Role "member"
+ }
